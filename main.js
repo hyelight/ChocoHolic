0abfab9f65920ae9b42bc39af0915a5011efb97f
hyelight <zheldeld@gmail.com>
2023-03-30 17:45:26 +0900

    info 섹션 반응형

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Family Site 아코디언
 const accordionHeader = document.querySelector(".accordion-header");
 const accordionContent = document.querySelector(".accordion-content");
 
@@ -10,4 +11,44 @@ accordionHeader.addEventListener("click", function () {
   } else {
     accordionContent.style.display = "none";
   }
+});
+
+// info-content 클래스들의 p 선택 시 아코디언
+const infoContents = document.querySelectorAll(".info-content p");
+
+function toggleUlAccordion(e) {
+  if (window.innerWidth <= 830) {
+    const content = e.target.nextElementSibling;
+    content.classList.toggle(".active");
+
+    if (content.classList.contains(".active")) {
+      content.style.display = content.className.includes("icons-wrapper")
+        ? "flex"
+        : "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
+}
+
+infoContents.forEach((infoContent) => {
+  infoContent.addEventListener("click", toggleUlAccordion);
+});
+
+// opening-time 개별 아코디언 설정
+const openingTimeHeader = document.querySelector(".opening-time p");
+const infoContentEachWrapper = document.querySelectorAll(
+  ".info-content-each-wrapper"
+);
+
+openingTimeHeader.addEventListener("click", function () {
+  infoContentEachWrapper.forEach((wrapper) => {
+    wrapper.classList.toggle("active");
+
+    if (wrapper.classList.contains("active")) {
+      wrapper.style.display = "flex";
+    } else {
+      wrapper.style.display = "none";
+    }
+  });
 });
